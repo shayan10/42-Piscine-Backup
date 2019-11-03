fa0ced34cd2b7d3125f06ff4590a92142a12ecdd
Shayan Khan <shayankhan283@gmail.com>
2019-11-02 21:55:57 -0700

    ex00

--- ex00/ft_split_whitespaces.c
+++ ex00/ft_split_whitespaces.c
@@ -40,8 +40,12 @@ char **ft_split_whitespaces(char *str)
     char **arr;
     int *lengths;
     int i;
+    int j;
+    int k;
 
     i = 0;
+    j = 0;
+    k = 0;
     lengths = word_length(str);
     //Allocating memory
     arr = (char **)malloc(sizeof(char *) * (word_count(str) + 1));
@@ -49,6 +53,24 @@ char **ft_split_whitespaces(char *str)
     {
         arr[i] = malloc(sizeof(char) * (lengths[i] + 1));
         i++;
+    }
+    i = 0;
+    while (*str != '\0')
+    {
+        if (*str == '\t' || *str == '\n' || *str == '\0' || *str == ' ')
+        {
+            i++;
+            k++;
+        }
+        else
+        {
+            while (j < lengths[k])
+            {
+                arr[i][j] = *str;
+                j++;
+            }
+        }
+        str++;
     }
     return (arr);
 }
